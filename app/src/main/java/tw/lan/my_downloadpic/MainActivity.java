42c02305b80f4c22f7b0173d515650f05d5dc262
johnnylan168 <johnnylan168@gmail.com>
2017-03-19 02:12:16 +0800

    下載圖片(Compress)

--- app/src/main/java/tw/lan/my_downloadpic/MainActivity.java
+++ app/src/main/java/tw/lan/my_downloadpic/MainActivity.java
@@ -1,13 +1,38 @@
 package tw.lan.my_downloadpic;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mDownloadPicCompress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        processView();
     }
+
+    private void processView() {
+        mDownloadPicCompress = (Button) findViewById(R.id.downloadPic_compress);
+        mDownloadPicCompress.setOnClickListener(buttonClickListener);
+    }
+
+    View.OnClickListener buttonClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent();
+            switch (v.getId()) {
+                case R.id.downloadPic_compress:
+                    intent.setClass(MainActivity.this, DownloadPicCompressActivity.class);
+                    break;
+            }
+            startActivity(intent);
+        }
+    };
 }
